2d3fddcc29c27ffd244b553fde6556eed92303aa
moonrailgun <moonrailgun@gmail.com>
2016-08-29 14:10:09 +0800

    ionic config
    UPDATE ionic config

--- www/js/app.js
+++ www/js/app.js
@@ -6,9 +6,8 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
-
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -20,10 +19,18 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
         // org.apache.cordova.statusbar required
         StatusBar.styleDefault();
       }
+
+      $ionicConfigProvider.tabs.position('bottom');
+      $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+      $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+      $ionicConfigProvider.backButton.previousTitleText(false);
+      $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+      $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+      $ionicConfigProvider.platform.ios.views.transition('ios');
+      $ionicConfigProvider.platform.android.views.transition('android');
     });
   })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -31,70 +38,69 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
     // Each state's controller can be found in controllers.js
     $stateProvider
 
-      // setup an abstract state for the tabs directive
+    // setup an abstract state for the tabs directive
       .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
 
-      // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'DashCtrl'
-          }
+    .state('tab.dash', {
+      url: '/dash',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
         }
-      })
+      }
+    })
 
-      .state('tab.shop', {
-        url: '/shop',
-        views: {
-          'tab-shop': {
-            templateUrl: 'templates/tab-shop.html',
-            controller: 'ShopCtrl'
-          }
+    .state('tab.shop', {
+      url: '/shop',
+      views: {
+        'tab-shop': {
+          templateUrl: 'templates/tab-shop.html',
+          controller: 'ShopCtrl'
         }
-      })
+      }
+    })
 
-      .state('tab.shop-item-detail', {
-        url: '/shop/:itemId',
-        views: {
-          'tab-shop': {
-            templateUrl: 'templates/shop-item-detail.html',
-            controller: 'ShopItemDetailCtrl'
-          }
+    .state('tab.shop-item-detail', {
+      url: '/shop/:itemId',
+      views: {
+        'tab-shop': {
+          templateUrl: 'templates/shop-item-detail.html',
+          controller: 'ShopItemDetailCtrl'
         }
-      })
+      }
+    })
 
-      .state('tab.download', {
-        url: '/download',
-        views: {
-          'tab-download': {
-            templateUrl: 'templates/tab-download.html',
-            controller: 'DownloadCtrl'
-          }
+    .state('tab.download', {
+      url: '/download',
+      views: {
+        'tab-download': {
+          templateUrl: 'templates/tab-download.html',
+          controller: 'DownloadCtrl'
         }
-      })
+      }
+    })
 
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
-          }
+    .state('tab.account', {
+      url: '/account',
+      views: {
+        'tab-account': {
+          templateUrl: 'templates/tab-account.html',
+          controller: 'AccountCtrl'
         }
-      });
+      }
+    });
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/dash');
 
   })
-
   .constant('$ionicLoadingConfig', {
     content: '正在载入中...',
     animation: 'fade-in',
